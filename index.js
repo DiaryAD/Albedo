1ad5a92a16c5e85c41b579103eec449e5d8e15a7
DiaryAD <113698563+DiaryAD@users.noreply.github.com>
2022-11-30 14:09:41 +0100

    Main1.0.01 | main structure with events

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
+const eventsPath = path.join(__dirname, 'events');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
@@ -15,22 +16,14 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-});
+}
 
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
 client.login(token);

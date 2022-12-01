26b231df7c1e99545502558867ac55c9b87c638a
DiaryAD <113698563+DiaryAD@users.noreply.github.com>
2022-12-01 09:36:05 +0100

    feat(commands): added a ban command
    also renamed deploy-commands.js into deploy.js
    and also renamed server.js into serverstats.js
    
    version: Main:1.2.00

---
+++ commands/ban.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Select a member and ban them.')
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('The member to ban')
+                .setRequired(true))
+        .addStringOption(option =>
+            option
+                .setName('reason')
+                .setDescription('The reason for banning'))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .setDMPermission(false),
+
+    async execute(interaction) {
+        const target = interaction.options.getUser('target');
+        const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+        await interaction.reply({content:`Banning ${target.username} for reason: ${reason}`, ephemeral:true});
+        await interaction.guild.members.ban(target)
+    },
+}
--- commands/serverstats.js
+++ commands/serverstats.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('server')
+        .setName('serverstats')
         .setDescription('Provides information about the server.'),
     async execute(interaction) {
         await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
